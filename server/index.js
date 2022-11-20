2a8c34b4f3be57f94cbecb1b814be5d8e57a8209
reytiartur <reyti.artur@gmail.com>
2022-11-20 18:15:50 +0100

    Heroku deployment configuration

--- server/index.js
+++ server/index.js
@@ -3,13 +3,20 @@ const videos = require('./data')
 const express = require("express");
 const fs = require('fs')
 
-// fs.writeFileSync('data.json')
-
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
+app.get("/videos", (req, res) => {
     res.json(videos);
 });
 
