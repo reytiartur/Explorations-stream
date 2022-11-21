0466285bcf78bb9e98558159e663dfdc558e7edf
reytiartur <reyti.artur@gmail.com>
2022-11-21 16:43:15 +0100

    Configurating for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -1,25 +1,18 @@
 const videos = require('./data')
 
 const express = require("express");
-const fs = require('fs')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
 
-
-app.get("/videos", (req, res) => {
+app.get("/", (req, res) => {
     res.json(videos);
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+module.exports = app;
